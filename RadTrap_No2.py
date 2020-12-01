Fixed issues with reabsortion coefficient calculation - now producing the same results as Boffard

RadTrap_No2.py
@@ -11,17 +11,17 @@ import pandas as pd
 import csv
 import os
 import time
-from Reab_Calc import reab_coeff_calc
+from math import pi
 
 from datetime import datetime
 datestring = datetime.strftime(datetime.now(), '%Y-%m-%d-%H-%M-%S')
 
-T_g = 600 #757 = 15mtorr #gas temperature (K)
-p = 10 #charactersitic readsorption length (cm)
-#M = 9.109E-31
-M = 39.948# 6.6335209E-23 #kg
-#M = 6.6335209E-26 #atomic mass of Ar(kg)
-R = 8.31446261815324*(1E4)
+# T_g = 600 #757 = 15mtorr #gas temperature (K)
+# p = 5 #charactersitic readsorption length (cm)
+# #M = 9.109E-31
+# M = 39.948# 6.6335209E-23 #kg
+# #M = 6.6335209E-26 #atomic mass of Ar(kg)
+# R = 8.31446261815324*(1E3)
 
 # n_1s3 = n_1s5/6.5
 # n_1s2 = n_1s4
@@ -54,23 +54,61 @@ def LR_750(k,T,a,E,km,am,Em,kr,ar,Er):
     sum_750=sum_nk(kG_750,T,alphaG_750,EG_750,kM_750,alphaM_750,EM_750,kR_750,alphaR_750,ER_750) 
     
 def LR_mod(k,T,a,E,km,am,Em,kr,ar,Er,Rij,Rmn):
-    LR = Rij/Rmn(sum_nk(k,T,a,E,km,am,Em,kr,ar,Er)/LR_750(k,T,a,E,km,m,kr,ar,Er))
+    LR = Rij/Rmn(sum_nk(k,T,a,E,km,am,Em,kr,ar,Er)/LR_750(k,T,a,E,km,am,kr,ar,Er))
 
-n_ij, A, g_i, g_j, n_j = np.loadtxt("line_data2.txt", comments='#', delimiter=';', skiprows=2, unpack=True, 
-                                        usecols=(0,1,2,3,4))
+#n_ij, A, g_i, g_j, n_j = np.loadtxt("line_data2.txt", comments='#', delimiter=';', skiprows=2, unpack=True, 
+ #                                       usecols=(0,1,2,3,4))
 
 #R738 = ((escape_factor(df.at[7,"k_o"],df.at[7,"n_j"],p))*(df.at[7,"A"] + 
 #             df.at[6,"A"]+df.at[8,"A"]))/(((escape_factor(df.at[7,"k_o"],df.at[7,"n_j"],p))*df.at[7,"A"]) + 
 #                  ((escape_factor(df.at[6,"k_o"],df.at[6,"n_j"],p))*df.at[6,"A"]) +
 #                  (escape_factor(df.at[8,"k_o"],df.at[8,"n_j"],p)*df.at[8,"A"]))
 
+def ko_calc(lamda,gi,gj,Aij,M,R):
+    term1 = ((lamda*(1E-7))**3)/(8*(np.pi**(1.5)))
+    #print("term 1 =",term1)
 
-def radtrap(n,A,gi,gj,nj):
+    term2 = gi/gj
+    #print("term 2 =", term2)
+    
+    term3 = Aij*(sqrt(M))/((sqrt(2)*(sqrt(R))))
+    #print("term 3 =", term3)
+    
+    result = term1*term2*term3
+    
+    #arr_k = np.array(result)
+    
+    return result 
+
+def ko_explicit(lamda,gi,gj,Aij,M,R):
+    T1 = (((lamda*1E-9)**3)/(8*(pi**(3/2))))*(gi/gj)*Aij*((sqrt(M)/sqrt(2)))
+    T2 = (T1*((R)**(-0.5)))*(1E4)
+    
+    print("T1: ", T1)
+    print("T2 = ", T2)
+    
+    return T2
+
+def reab_coeff_calc(n_ij,g_i,g_j,A,n_j,T_g,M,R):
+        with open("line_data_full.txt","a+") as datafile:      
+            for a, b, c, d, e in zip(n_ij,g_i,g_j,A,n_j):
+                #print("ko for ", a, "nm is: ",ko_calc(a,b,c,d))
+                ko = ko_explicit(a,b,c,d,M,R) 
+                kij = ko*e*((T_g)**(-0.5))
+                #print( " and kij = ", kij)
+                #print(" ")
+                
+                datafile.write("%6.2f %1.0f %1.0f %5.2e %7.2e %7.2e %7.5f \n" % (a,b,c,d,e,ko,kij))
+
+def radtrap(n,A,gi,gj,nj,p,T_g,M,R):
     
     print("Calculating absorption coefficients...")
     print("Fetching ko data from Reab_Calc.py")
     
-    reab_coeff_calc(n, gi, gj, A, nj)
+    # with open("line_data_full.txt","w+") as datafile:
+    #     datafile.write('lamda_ij g_i g_j A_ij ko_ij, k_ij \n')
+    
+    reab_coeff_calc(n, gi, gj, A, nj,T_g,M,R)
     
     print("")
     print("ko and kij calculated and printed to file.")
@@ -91,17 +129,17 @@ def radtrap(n,A,gi,gj,nj):
     #2p1 750.39, 667.73
     
     #with open("RadTrap_Coeff.csv","w+") as radfile:
-    print(" ")
-    print("The escape factor for 738 is:",escape_factor(df.at[7,"k_ij"],df.at[7,"n_j"],p))
-    print("g*A for 738 is:",escape_factor(df.at[7,"k_ij"],df.at[7,"n_j"],p)*(df.at[7,"A"] + 
-             df.at[6,"A"]+df.at[8,"A"]))
+    # print(" ")
+    # print("The escape factor for 738 is:",escape_factor(df.at[7,"k_ij"],df.at[7,"n_j"],p))
+    # print("g*A for 738 is:",escape_factor(df.at[7,"k_ij"],df.at[7,"n_j"],p)*(df.at[7,"A"] + 
+    #          df.at[6,"A"]+df.at[8,"A"]))
         
     R_750 = (((escape_factor(df.at[0,"k_ij"],df.at[0,"n_j"],p))*((df.at[0,"A"]) + 
              df.at[1,"A"])))/(((escape_factor(df.at[0,"k_ij"],df.at[0,"k_ij"],p))*df.at[0,"A"]) + 
                   (escape_factor(df.at[1,"k_ij"],df.at[1,"n_j"],p))*df.at[1,"A"])
-    print(" ") 
-    print("R_750 is:")                        
-    print(R_750)
+    # print(" ") 
+    # print("R_750 is:")                        
+    # print(R_750)
     
     #2p2 826.45, 772.42, 727.29, 696.54
 
@@ -164,7 +202,7 @@ def radtrap(n,A,gi,gj,nj):
     Rad_arr = np.array(Rad)
     Rad_lam_arr = np.array(R_lam)
     
-    print(" ")
+    # print(" ")
     df2 = pd.DataFrame(list(zip(R_lam,Rad)),columns=["Wavelength","Radation Trapping Coefficient"])
     df2.to_csv(r'Rad_TrapCoeff.csv', sep=';', index = False)
     print("Radiation trapping coefficients, along with their corresponding ")
@@ -177,50 +215,54 @@ def radtrap(n,A,gi,gj,nj):
     #print(df)
     #print(df2)
     #print(EF_all)
-    print(" ")
-    print(" !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!")
-    print("The ko results are: ")
-    print(df)
-    print(" ")
-    print("The RadTrap results are: ")
-    print(df2)
-    
+    # print(" ")
+    # print(" !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!")
+    # print("The ko results are: ")
+    # print(df)
+    # print(" ")
+    # print("The RadTrap results are: ")
+    # print(df2)
+       
     return Rad, df
     #return R_750, R_772_4, R_738, R_794, R_751, R_763, R_772_3
     
+#n_ij, g_i, g_j, A, n_j, ko, k_ij = np.loadtxt("line_data_full.txt", comments='#', delimiter=' ', unpack=True, 
+ #                                        usecols=(0,1,2,3,4,5,6))
 #radtrap(n_ij,A,g_i,g_j,n_j)
 
 #Writing escape factors for each line to file
 
-n_ij, g_i, g_j, A, n_j, ko, k_ij = np.loadtxt("line_data_full.txt", comments='#', delimiter=' ', unpack=True, 
-                                         usecols=(0,1,2,3,4,5,6))
+#n_ij, g_i, g_j, A, n_j, ko, k_ij = np.loadtxt("line_data_full.txt", comments='#', delimiter=' ', unpack=True, 
+ #                                        usecols=(0,1,2,3,4,5,6))
 
 #csv_input = pd.read_csv("line_data_full.txt",sep=" ",header=None,names=['Wavelength (nm)','g_i','g_j','A','n_j','k_o', 'k_ij'],comment="#")
 
 #csv_input['Radiation Trapping Coefficient'] = csv_input['Radiation Trapping Coefficient']
 #csv_input.to_csv('output.csv', index=False)
+    
+def print_escape(n_ij,g_i,g_j,A,n_j,ko,k_ij,p):
+    
+    with open("line_escape_factors.txt","w+") as datafile:
+        for a, b, c, d, e, f, g in zip(n_ij,g_i,g_j,A,n_j,ko,k_ij):
+            g_ij = escape_factor(g,e,p)
+            #print("escape factor for ",a, "nm : ")
+            #print(g_ij)
+            #print("--------")
+            gA = g_ij*d
+            datafile.write("%6.2f %1.0f %1.0f %5.2e %7.2e %7.2e %7.5f %5.3f %3.5f \n" % (a,b,c,d,e,f,g,gA,g_ij))
+         
+    n_ij, g_i, g_j, A, n_j, ko, k_ij, gA, g_ij = np.loadtxt("line_escape_factors.txt", comments='#', delimiter=' ', unpack=True, 
+                                             usecols=(0,1,2,3,4,5,6,7,8))
+#print(" split arrays are:")
+#newA = np.array_split(A,[1,4,7,11,13,16,])
 
-with open("line_escape_factors.txt","w+") as datafile:
-    for a, b, c, d, e, f, g in zip(n_ij,g_i,g_j,A,n_j,ko,k_ij):
-        g_ij = escape_factor(g,e,p)
-        print("escape factor for ",a, "nm : ")
-        print(g_ij)
-        print("--------")
-        gA = g_ij*d
-        datafile.write("%6.2f %1.0f %1.0f %5.2e %7.2e %7.2e %7.5f %5.3f %3.5f \n" % (a,b,c,d,e,f,g,gA,g_ij))
-     
-n_ij, g_i, g_j, A, n_j, ko, k_ij, gA, g_ij = np.loadtxt("line_escape_factors.txt", comments='#', delimiter=' ', unpack=True, 
-                                         usecols=(0,1,2,3,4,5,6,7,8))
-print(" split arrays are:")
-newA = np.array_split(A,[1,4,7,11,13,16,])
-
-print("2p1 array = ", newA[0])
-print("2p2 array = ", newA[1])
-print("2p3 array = ", newA[2])
-print("2p4 array = ", newA[3])
-print("2p5 array = ", newA[4])
-print("2p6 array = ", newA[5])
-print("2p7 array = ", newA[6])
+# print("2p1 array = ", newA[0])
+# print("2p2 array = ", newA[1])
+# print("2p3 array = ", newA[2])
+# print("2p4 array = ", newA[3])
+# print("2p5 array = ", newA[4])
+# print("2p6 array = ", newA[5])
+# print("2p7 array = ", newA[6])
 
         
         # =(g*SUM(A))/(SUM(gA))

Reab_Calc.py
@@ -11,12 +11,12 @@ import time
 
 #Constants
 
-T_g = 600 #757 = 15mtorr #gas temperature (K)
-p = 10 #charactersitic readsorption length (cm)
-#M = 9.109E-31
-M = 39.948# 6.6335209E-23 #kg
-#M = 6.6335209E-26 #atomic mass of Ar(kg)
-R = 8.31446261815324*(1E4)
+# T_g = 600 #757 = 15mtorr #gas temperature (K)
+# p = 5 #charactersitic readsorption length (cm)
+# #M = 9.109E-31
+# M = 39.948# 6.6335209E-23 #kg
+# #M = 6.6335209E-26 #atomic mass of Ar(kg)
+# R = 8.31446261815324*(1E4)
 
 
 #Function to calculate the reabsorption coefficients k_o for each of the 22 lines
@@ -70,8 +70,9 @@ def ko_calc(lamda,gi,gj,Aij):
 
 #n_ij, A, g_i, g_j, n_j, ko, k_ij = np.loadtxt("line_data_full.txt", comments='#', delimiter=' ', unpack=True, 
                                    #usecols=(0,1,2,3,4,5,6))   
-n_ij, A, g_i, g_j, n_j = np.loadtxt("line_data2.txt", comments='#', delimiter=';', skiprows=2, unpack=True, 
-                                    usecols=(0,1,2,3,4))
+#n_ij, A, g_i, g_j = np.loadtxt("line_data2.txt", comments='#', delimiter=';', skiprows=2, unpack=True, 
+                                    #usecols=(0,1,2,3))
+#n_j = np.loadtxt("live_density.txt",comments='#',delimiter=' ', usecols=(2))
 
 #n_ij = [750.39,667.73]
 #A = [47200000,235000]
@@ -85,15 +86,14 @@ n_ij, A, g_i, g_j, n_j = np.loadtxt("line_data2.txt", comments='#', delimiter=';
 #print("ko data with 750 and 738")
 #print(ko_data(n_ij,g_i,g_j,A,nj))
 
-def reab_coeff_calc(n,gi,gj,A,nj):
-        with open("line_data_full.txt","w+") as datafile:
-        
+def reab_coeff_calc(n_ij,g_i,g_j,A,n_j):
+        with open("line_data_full.txt","w+") as datafile:      
             for a, b, c, d, e in zip(n_ij,g_i,g_j,A,n_j):
-                print("ko for ", a, "nm is: ",ko_calc(a,b,c,d))
+                #print("ko for ", a, "nm is: ",ko_calc(a,b,c,d))
                 ko = ko_calc(a,b,c,d) 
                 kij = ko*e*((T_g)**(-0.5))
-                print( " and kij = ", kij)
-                print(" ")
+                #print( " and kij = ", kij)
+                #print(" ")
                 
                 datafile.write("%6.2f %1.0f %1.0f %5.2e %7.2e %7.2e %7.5f \n" % (a,b,c,d,e,ko,kij))
     

combo.py
@@ -30,7 +30,7 @@ Kb = 1.38E-23*(1E4) #boltzman constant
 #M = 9.109E-31
 M = 39.948# 6.6335209E-23 #kg
 #M = 6.6335209E-26 #atomic mass of Ar(kg)
-R = 8.31446261815324*(1E4)
+R = 8.31446261815324*(1E3)
 T_g = 600 #757 = 15mtorr #gas temperature (K)
 p = 5 #charactersitic readsorption length (cm)
 
@@ -45,7 +45,7 @@ p_str = str(p)
 #Process Pressure, n_m, n_r
 
 #List of datafiles to be used in electron density calculations.
-filelist = ['LOCAL_RF_POS10006']
+filelist = ['LOCAL_RF_POS10001']
             # 'LOCAL_RF_POS10002',
             # 'LOCAL_RF_POS10003',
             # 'LOCAL_RF_POS10004',
@@ -59,6 +59,7 @@ filelist = ['LOCAL_RF_POS10006']
             # 'LOCAL_RF_POS10012']
 
 #Transition Probabilties and reabsorption coeffcients for 6 Ar lines
+#for BF analysis 
 #units for kij0 = cm^2K^0.5
 #units for Aij = s^-1
 
@@ -203,7 +204,7 @@ def reabs_coeff(k,n):
 #Function for calculation of escape factors
 
 def escape_factor(k,n,p):
-    return (2-exp(-reabs_coeff(k,n)*p/1000))/(1+(reabs_coeff(k,n)*p))
+    return (2-exp((-reabs_coeff(k,n)*p)/1000))/(1+(reabs_coeff(k,n)*p))
 
 #Function for calculation of Model Line Ratios
                 
@@ -356,42 +357,48 @@ def Te(T,n_m,n_r):
     n_ij, A, g_i, g_j = np.loadtxt("line_data2.txt", comments='#', delimiter=';', skiprows=2, unpack=True, 
                                         usecols=(0,1,2,3))
     
-    n_i = np.loadtxt("line_data2.txt", comments='#',dtype=str, delimiter=';', skiprows=2, unpack=True, 
+    j_level = np.loadtxt("line_data2.txt", comments='#',dtype=str, delimiter=';', skiprows=2, unpack=True, 
                                         usecols=(4))
 
     #Taking n_m and n_r from calculation 
     n_1s3 = n_m/6.5
     n_1s2 = n_r
     
-    for a, b in zip(n_ij,n_i):
+    for a, b, c, d, e in zip(n_ij,A,g_i,g_j,j_level):
         print("Wavelength: ", a)
-        print("Lower level: ", b)
-        if b=="1s2":
+        print("A: ", b)
+        print("g_i", c)
+        print("g_j", d)
+        print("Lower level: ", e)
+        if e=="1s2":
             N_j = n_1s2
-        elif b=="1s3":
+        elif e=="1s3":
             N_j = n_1s3
-        elif b=="1s4":
+        elif e=="1s4":
             N_j = n_r
-        elif b=="1s5":
+        elif e=="1s5":
             N_j = n_m
         
-        print("Density of ", b, "is ", N_j)
+        print("Density of ", e, "is ", N_j)
             
         with open('live_density.txt', 'a+') as resultsfile:
-            resultsfile.write("%4.2f %s %4.2e \n" % (a,b,N_j))
+            resultsfile.write("%4.2f %4.2f %3.1f %3.1f %4.2e \n" % (a,b,c,d,N_j))
     
-    n_ij, n_j = np.loadtxt("live_density.txt", comments='#', delimiter=' ', unpack=True, 
-                                        usecols=(0,2))  
+    n_ij, A, g_i, g_j, n_j = np.loadtxt("live_density.txt", comments='#', delimiter=' ', unpack=True, 
+                                        usecols=(0,1,2,3,4))
+    
+    os.rename("live_density.txt", ("live_density"+i+param+Tg+'.txt'))
     
     
-    radtrap(n_ij,A,g_i,g_j,n_j)
+    radtrap(n_ij,A,g_i,g_j,n_j,p,T_g,M,R)
     print("")
     print("radtrap complete")
     print("")
     
     n_ij, g_i, g_j, A, n_j, ko, k_ij = np.loadtxt("line_data_full.txt", comments='#', delimiter=' ', unpack=True, 
                                    usecols=(0,1,2,3,4,5,6))   
-    print_escape(n_ij,g_i,g_j,A,n_j,ko,k_ij)
+    print_escape(n_ij,g_i,g_j,A,n_j,ko,k_ij,p)
+    
     print("")
     print("escape complete")
     print("")
@@ -581,7 +588,7 @@ def e_density(T,i):
     nec_419 = 3.2E11 #calculated from reciprocal of transition probability
     nec_367 = 1E11 #estimate based on Zhu 2007
     nec_415 = 5E11
-    nec_383 = 1E11 #Zhu 2007
+    nec_383 = 9E10 #Zhu 2007 and Wang
     nec_365 = 1E11
     nec_360 = 1E11
     
@@ -626,15 +633,19 @@ def e_density(T,i):
 #filename = input("What is the name of the file you want to analyse?")
     
 for i in filelist:
-        # PP_mbar = float(input("What is the process pressure in mbar?"))
-        # #PP_mbar = 0.0020 #argon partial pressure in mbar
-        # PP_pa = PP_mbar*100 #argon partial pressure in pascals
-        # n_g = PP_pa/(Kb*T_g)#6E13 
+        PP_mbar = float(input("What is the process pressure in mbar?"))
+        #PP_mbar = 0.0020 #argon partial pressure in mbar
+        PP_pa = PP_mbar*100 #argon partial pressure in pascals
+                
+        PP_mtorr = PP_mbar/0.00133
+        n_g = ((3.54E13)*PP_mtorr*(273/T_g))#PP_mtorr/(Kb*T_g)#6E13 
+        
+        print("the neutral density, calculated using PP, is: ", "%4.2e" % n_g)
         
         #New n_g
         #Assuming that p=0.0050mbar, which is 3.75mtorr, then at 0.5kW, n_g = 6E13
-        N_g = float(input("What is the ground state density, in x10^13 cm^-3"))
-        n_g = N_g*(1E13)
+        # N_g = float(input("What is the ground state density, in x10^13 cm^-3"))
+        # n_g = N_g*(1E13)
         
         #Ask user for electon temperature value to be used in electron density calculation
         #calling integrate function to determine intensity of 451 and 750 emission lines
@@ -669,15 +680,15 @@ for i in filelist:
         #Executing function to calculate n_m and n_r
         final_density = LR(n1s4,n1s5)
         
+        #Calculation of electron temperature by looping through Te values
+        #------------------
+        #Te Calculation
+        #-------------------------------------------------------
         print("--------------------------------------------------------------")
         print("")
         print("You are now calculating electron temperature")
         print("")
-        
-        #------------------
-        #Te Calculation
-               
-        #-------------------------------------------------------
+                             
         #Loading Te model data 
         
         #Extract data and declare variables