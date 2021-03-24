Successful integration of electron temperature calculation into refactored code. Produces correct results for a 1.5kW plasma example. All files have been integrated, but not all have been refactored.

RadTrapAndEscapeFactorCalculation.py
@@ -8,61 +8,18 @@ import numpy as np
 from math import exp
 from math import sqrt
 import pandas as pd
-import csv
-import os
-import time
 from math import pi
 
 from datetime import datetime
 datestring = datetime.strftime(datetime.now(), '%Y-%m-%d-%H-%M-%S')
 
-# T_g = 600 #757 = 15mtorr #gas temperature (K)
-# p = 5 #charactersitic readsorption length (cm)
-# #M = 9.109E-31
-# M = 39.948# 6.6335209E-23 #kg
-# #M = 6.6335209E-26 #atomic mass of Ar(kg)
-# R = 8.31446261815324*(1E3)
-
-# n_1s3 = n_1s5/6.5
-# n_1s2 = n_1s4
-
-#Extract data from original file, calculate k_o values and then extract full data,
-#including n_ij, A, g_i, g_j, n_j and k_o
-
-#Function for caluclation of Excitation rates
-
-def ex_rates(k,T,a,E):
-    return k*((T)**a)*exp(-E/T)
-
-def sum_nk(k,T,a,E,km,am,Em,kr,ar,Er):    
-    return n_g*ex_rates(k,T,a,E) + n_m*ex_rates(km,T,am,Em) +  n_r*ex_rates(kr,T,ar,Er)
-
-#def reabs_coeff(k,n):
- #   return k*n*(T_g**-0.5)
 
 #Function for calculation of escape factors, g
 
 #swapped reabs_coeff for k
 def escape_factor(k,n,p):
+    p = int(p)
     return (2-exp(-k*p/1000))/(1+(k*p))
-
-def rad_trap(k_ij,p,Ag,Am,Ar,kg,km,kr):
-    return (escape_factor(k,n,p)*(Ag+Am+Ar))/((Ag*escape_factor(kg,n,p))+
-            (Am*escape_factor(k,n,p))+(Ar*escape_factor(k,n,p)))
-    
-def LR_750(k,T,a,E,km,am,Em,kr,ar,Er):
-    sum_750=sum_nk(kG_750,T,alphaG_750,EG_750,kM_750,alphaM_750,EM_750,kR_750,alphaR_750,ER_750) 
-    
-def LR_mod(k,T,a,E,km,am,Em,kr,ar,Er,Rij,Rmn):
-    LR = Rij/Rmn(sum_nk(k,T,a,E,km,am,Em,kr,ar,Er)/LR_750(k,T,a,E,km,am,kr,ar,Er))
-
-#n_ij, A, g_i, g_j, n_j = np.loadtxt("line_data2.txt", comments='#', delimiter=';', skiprows=2, unpack=True, 
- #                                       usecols=(0,1,2,3,4))
-
-#R738 = ((escape_factor(df.at[7,"k_o"],df.at[7,"n_j"],p))*(df.at[7,"A"] + 
-#             df.at[6,"A"]+df.at[8,"A"]))/(((escape_factor(df.at[7,"k_o"],df.at[7,"n_j"],p))*df.at[7,"A"]) + 
-#                  ((escape_factor(df.at[6,"k_o"],df.at[6,"n_j"],p))*df.at[6,"A"]) +
-#                  (escape_factor(df.at[8,"k_o"],df.at[8,"n_j"],p)*df.at[8,"A"]))
 
 def ko_calc(lamda,gi,gj,Aij,M,R):
     term1 = ((lamda*(1E-7))**3)/(8*(np.pi**(1.5)))
@@ -83,9 +40,8 @@ def ko_calc(lamda,gi,gj,Aij,M,R):
 def ko_explicit(lamda,gi,gj,Aij,M,R):
     T1 = (((lamda*1E-9)**3)/(8*(pi**(3/2))))*(gi/gj)*Aij*((sqrt(M)/sqrt(2)))
     T2 = (T1*((R)**(-0.5)))*(1E4)
-    
-    print("T1: ", T1)
-    print("T2 = ", T2)
+    # print("T1: ", T1)
+    # print("T2 = ", T2)
     
     return T2
 
@@ -105,9 +61,6 @@ def radtrap(n,A,gi,gj,nj,p,T_g,M,R):
     print("Calculating absorption coefficients...")
     print("Fetching ko data from Reab_Calc.py")
     
-    # with open("line_data_full.txt","w+") as datafile:
-    #     datafile.write('lamda_ij g_i g_j A_ij ko_ij, k_ij \n')
-    
     reab_coeff_calc(n, gi, gj, A, nj,T_g,M,R)
     
     print("")
@@ -118,89 +71,45 @@ def radtrap(n,A,gi,gj,nj,p,T_g,M,R):
     
     print(" ")
     print("Absorption coefficients calculated.")
-    
-    #with open("k_oresults.txt","r") as f:
-    
-    #datafile = open('k_oresults.txt', 'a+')       
-    
-    #Calculation of radiation trapping coefficients for each upper level, taking into account all transitions to
-    #the lower level of interest (with some minor exceptions)
-    
-    #2p1 750.39, 667.73
-    
-    #with open("RadTrap_Coeff.csv","w+") as radfile:
-    # print(" ")
-    # print("The escape factor for 738 is:",escape_factor(df.at[7,"k_ij"],df.at[7,"n_j"],p))
-    # print("g*A for 738 is:",escape_factor(df.at[7,"k_ij"],df.at[7,"n_j"],p)*(df.at[7,"A"] + 
-    #          df.at[6,"A"]+df.at[8,"A"]))
         
     R_750 = (((escape_factor(df.at[0,"k_ij"],df.at[0,"n_j"],p))*((df.at[0,"A"]) + 
              df.at[1,"A"])))/(((escape_factor(df.at[0,"k_ij"],df.at[0,"k_ij"],p))*df.at[0,"A"]) + 
                   (escape_factor(df.at[1,"k_ij"],df.at[1,"n_j"],p))*df.at[1,"A"])
-    # print(" ") 
-    # print("R_750 is:")                        
-    # print(R_750)
-    
-    #2p2 826.45, 772.42, 727.29, 696.54
-
     
     R_772_4 = (((escape_factor(df.at[3,"k_ij"],df.at[3,"n_j"],p))*(df.at[3,"A"] + 
              df.at[2,"A"] + df.at[4,"A"] + df.at[5,"A"])))/(((escape_factor(df.at[3,"k_ij"],df.at[3,"n_j"],p))*df.at[3,"A"]) + 
                   ((escape_factor(df.at[2,"k_ij"],df.at[2,"n_j"],p))*df.at[2,"A"]) + 
                   ((escape_factor(df.at[4,"k_ij"],df.at[4,"n_j"],p))*df.at[4,"A"]) + 
                   ((escape_factor(df.at[5,"k_ij"],df.at[5,"n_j"],p))*df.at[5,"A"]))
-    #print(R_772_4)
-    
-    #2p3 840.82, 738.4, 706.72
-
-    
+                                                            
     R_738 = ((escape_factor(df.at[7,"k_ij"],df.at[7,"n_j"],p))*(df.at[7,"A"] + 
              df.at[6,"A"]+df.at[8,"A"]))/(((escape_factor(df.at[7,"k_ij"],df.at[7,"n_j"],p))*df.at[7,"A"]) + 
                   ((escape_factor(df.at[6,"k_ij"],df.at[6,"n_j"],p))*df.at[6,"A"]) +
                   (escape_factor(df.at[8,"k_ij"],df.at[8,"n_j"],p)*df.at[8,"A"]))
-    #print(R_738)
-    
-    #2p4 852.14, 794.82, 747.12, 714.7
-
     
     R_794 = (((escape_factor(df.at[10,"k_ij"],df.at[10,"n_j"],p))*((df.at[10,"A"]) + 
              df.at[9,"A"] + df.at[11,"A"] + df.at[12,"A"])))/(((escape_factor(df.at[10,"k_ij"],df.at[10,"n_j"],p))*df.at[10,"A"]) + 
                   ((escape_factor(df.at[9,"k_ij"],df.at[9,"n_j"],p))*df.at[9,"A"]) + 
                   ((escape_factor(df.at[11,"k_ij"],df.at[11,"n_j"],p))*df.at[11,"A"]) + escape_factor(df.at[12,"k_ij"],df.at[12,"n_j"],p))
-    #print(R_794)
-    
-    #2p5 857.81, 751.47
                                                                  
     R_751 = (((escape_factor(df.at[14,"k_ij"],df.at[14,"n_j"],p))*(df.at[14,"A"] + 
              df.at[13,"A"])))/(((escape_factor(df.at[14,"k_ij"],df.at[14,"k_ij"],p))*df.at[14,"A"]) + 
                   (escape_factor(df.at[13,"k_ij"],df.at[13,"n_j"],p))*df.at[13,"A"])
-    #print(R_751)
-    
-    #2p6 992.45, 800.62, 763.51
-
-    
+  
     R_763 = (((escape_factor(df.at[17,"k_ij"],df.at[17,"n_j"],p))*(df.at[17,"A"] + 
              df.at[15,"A"] + df.at[16,"A"])))/(((escape_factor(df.at[17,"k_ij"],df.at[17,"k_ij"],p))*df.at[17,"A"]) + 
                   (escape_factor(df.at[15,"k_ij"],df.at[15,"n_j"],p))*df.at[15,"A"] + 
                   ((escape_factor(df.at[16,"k_ij"],df.at[16,"n_j"],p))*df.at[16,"A"]))
-    #print(R_763)
-    
-    #2p7 935.42, 866.79, 810.37, 772.38
     
     R_772_3 = ((escape_factor(df.at[21,"k_ij"],df.at[21,"n_j"],p))*(df.at[21,"A"] + 
              df.at[18,"A"] + df.at[19,"A"] + df.at[20,"A"]))/(((escape_factor(df.at[21,"k_ij"],df.at[21,"n_j"],p))*df.at[21,"A"]) + 
                   ((escape_factor(df.at[18,"k_ij"],df.at[18,"n_j"],p))*df.at[18,"A"]) +
                   (escape_factor(df.at[19,"k_ij"],df.at[19,"n_j"],p)*df.at[19,"A"]) + 
                   (escape_factor(df.at[20,"k_ij"],df.at[20,"n_j"],p)*df.at[20,"A"]))
-    #print(R_772_3)
     
     #print("")
     Rad = [R_750,R_772_4,R_738,R_794,R_751,R_763,R_772_3]
     R_lam = [750.39,772.42,738.4,794.82,751.47,763.51,772.38]
-    #for testing purposes
-    
-    Rad_arr = np.array(Rad)
-    Rad_lam_arr = np.array(R_lam)
     
     # print(" ")
     df2 = pd.DataFrame(list(zip(R_lam,Rad)),columns=["Wavelength","Radation Trapping Coefficient"])
@@ -208,92 +117,15 @@ def radtrap(n,A,gi,gj,nj,p,T_g,M,R):
     print("Radiation trapping coefficients, along with their corresponding ")
     print("wavelength have been printed to Rad_TrapCoeff.csv")
     
-    #with open("rad_trap.txt","w+") as datafile:
-     #   for a, b in zip(Rad_lam_arr, Rad_arr):
-      #      datafile.write("%3.1f %7.2e\n" % (a,b))
-    
-    #print(df)
-    #print(df2)
-    #print(EF_all)
-    # print(" ")
-    # print(" !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!")
-    # print("The ko results are: ")
-    # print(df)
-    # print(" ")
-    # print("The RadTrap results are: ")
-    # print(df2)
-       
     return Rad, df
-    #return R_750, R_772_4, R_738, R_794, R_751, R_763, R_772_3
-    
-#n_ij, g_i, g_j, A, n_j, ko, k_ij = np.loadtxt("line_data_full.txt", comments='#', delimiter=' ', unpack=True, 
- #                                        usecols=(0,1,2,3,4,5,6))
-#radtrap(n_ij,A,g_i,g_j,n_j)
-
-#Writing escape factors for each line to file
-
-#n_ij, g_i, g_j, A, n_j, ko, k_ij = np.loadtxt("line_data_full.txt", comments='#', delimiter=' ', unpack=True, 
- #                                        usecols=(0,1,2,3,4,5,6))
-
-#csv_input = pd.read_csv("line_data_full.txt",sep=" ",header=None,names=['Wavelength (nm)','g_i','g_j','A','n_j','k_o', 'k_ij'],comment="#")
-
-#csv_input['Radiation Trapping Coefficient'] = csv_input['Radiation Trapping Coefficient']
-#csv_input.to_csv('output.csv', index=False)
     
 def print_escape(n_ij,g_i,g_j,A,n_j,ko,k_ij,p):
     
     with open("line_escape_factors.txt","w+") as datafile:
         for a, b, c, d, e, f, g in zip(n_ij,g_i,g_j,A,n_j,ko,k_ij):
             g_ij = escape_factor(g,e,p)
-            #print("escape factor for ",a, "nm : ")
-            #print(g_ij)
-            #print("--------")
             gA = g_ij*d
             datafile.write("%6.2f %1.0f %1.0f %5.2e %7.2e %7.2e %7.5f %5.3f %3.5f \n" % (a,b,c,d,e,f,g,gA,g_ij))
          
     n_ij, g_i, g_j, A, n_j, ko, k_ij, gA, g_ij = np.loadtxt("line_escape_factors.txt", comments='#', delimiter=' ', unpack=True, 
                                              usecols=(0,1,2,3,4,5,6,7,8))
-#print(" split arrays are:")
-#newA = np.array_split(A,[1,4,7,11,13,16,])
-
-# print("2p1 array = ", newA[0])
-# print("2p2 array = ", newA[1])
-# print("2p3 array = ", newA[2])
-# print("2p4 array = ", newA[3])
-# print("2p5 array = ", newA[4])
-# print("2p6 array = ", newA[5])
-# print("2p7 array = ", newA[6])
-
-        
-        # =(g*SUM(A))/(SUM(gA))
-    
-
-#df=pd.read_csv("line_data_full.txt",sep=" ",header=None,names=['Wavelength (nm)','A','g_i','g_j','n_j','k_ij'],comment="#")
-                                                               
-#print(df)
-                                                               
-#EF_all=[escape_factor(df.at[0,"k_ij"],df.at[0,"n_j"],p),escape_factor(df.at[3,"k_ij"],df.at[3,"n_j"],p),escape_factor(df.at[7,"k_ij"],df.at[7,"n_j"],p),escape_factor(df.at[10,"k_ij"],df.at[10,"n_j"],p),escape_factor(df.at[12,"k_ij"],df.at[12,"n_j"],p),escape_factor(df.at[14,"k_ij"],df.at[14,"n_j"],p),escape_factor(df.at[17,"k_ij"],df.at[17,"n_j"],p)]
-
-#a = EF_all
-#print(EF_all)
-
-#print(R_list)
-#print("")
-#R = np.array(R_list)
-
-#R_lam = np.array(R_lamda)
-
-   # np.T([[R_arr]])
-#np.T([[n_ij]])   
-
-
-#for i in R_list:
- #   radfile.write("%7.2e\n" % (i))
-#print("printed radiation trapping coefficients to RadTrap_Coeff.txt")
-#print(R_arr)
-
-
-
-  
-  
-

Reab_Calc.py
@@ -22,7 +22,7 @@ import time
 #Function to calculate the reabsorption coefficients k_o for each of the 22 lines
 #that must be used to determine the contributuon by radiation trapping.
 
-def ko_calc(lamda,gi,gj,Aij):
+def ko_calc(lamda,gi,gj,Aij,M,R):
     term1 = ((lamda*(1E-7))**3)/(8*(np.pi**(1.5)))
     #print("term 1 =",term1)
 
@@ -38,90 +38,16 @@ def ko_calc(lamda,gi,gj,Aij):
     
     return result 
 
-# def ko_data(n,gi,gj,A,nj):
-
-#     with open("line_data_full.txt","w+") as datafile:
-#     #datafile = open('line_data_full.txt', 'w+')
-    
-#         for a, b, c, d, e in zip(n,A,gi,gj,nj):
-        
-#             #print("----------------------")
-#             #print("For spectral line n_ij =", a, "nm")
-#             #print("Where g_i =", b)
-#             #print("and g_j=", c)
-#             #print("n_j =", "%5.2e" % d)
-#             #print("And Aij =", "%5.2e" % d)
-#             #print("k_o = ", "%7.2e" % ko_calc(a,b,c,d))
-#             ko = ko_calc(a,b,c,d)
-#             k_ij = ko*e*((T_g)**0.5)
-
-#             print("for lamda = ", a, "ko = ", ko,"k_ij = ", k_ij)
-#                 #for i in range(len(n1s4)):
-#                  #   mod_results.write("%d %d %d\n" % (n1s4[i],n1s5[i],chi_sum))
-#             datafile.write("%6.2f %3.0f %3.1f %3.1f %7.2e %7.2e %7.5f\n" % (a,b,c,d,e,ko,k_ij))
-#                     #np.append(arr,arr_k,axis=1)       
-#         print("")                
-#         print("Calculated reabsorption coeffients an printed to file:line_data_full.txt")
-        
-    #return ko
-    
-    #n_ij, A, g_i, g_j, n_j, ko = np.loadtxt("line_data_full.txt", comments='#', delimiter=' ', unpack=True, 
-     #                              usecols=(0,1,2,3,4,5))    
-
-#n_ij, A, g_i, g_j, n_j, ko, k_ij = np.loadtxt("line_data_full.txt", comments='#', delimiter=' ', unpack=True, 
-                                   #usecols=(0,1,2,3,4,5,6))   
-#n_ij, A, g_i, g_j = np.loadtxt("line_data2.txt", comments='#', delimiter=';', skiprows=2, unpack=True, 
-                                    #usecols=(0,1,2,3))
-#n_j = np.loadtxt("live_density.txt",comments='#',delimiter=' ', usecols=(2))
-
-#n_ij = [750.39,667.73]
-#A = [47200000,235000]
-#g_i = [1.0,1.0]
-#g_j = [3.0,3.0]
-#nj = [1.20E10,1.20E10]
-
-#print("!!!!!!!!!!!!! TESTING !!!!!!!!!!!!!!")
-#print ("!!!!!!!!!!!!!!!!!!!!!!!!!!!!!")
-
-#print("ko data with 750 and 738")
-#print(ko_data(n_ij,g_i,g_j,A,nj))
-
-def reab_coeff_calc(n_ij,g_i,g_j,A,n_j):
+def reab_coeff_calc(n_ij,g_i,g_j,A,n_j,T_g,M,R):
         with open("line_data_full.txt","w+") as datafile:      
-            for a, b, c, d, e in zip(n_ij,g_i,g_j,A,n_j):
+            for Wavelength, StatisticalWeightUpper,StatisticalWeightLower,Probability,LowerDensity in zip(n_ij,g_i,g_j,A,n_j):
                 #print("ko for ", a, "nm is: ",ko_calc(a,b,c,d))
-                ko = ko_calc(a,b,c,d) 
-                kij = ko*e*((T_g)**(-0.5))
+                ko = ko_calc(Wavelength,StatisticalWeightUpper,StatisticalWeightLower,Probability,M,R) 
+                kij = ko*LowerDensity*((T_g)**(-0.5))
                 #print( " and kij = ", kij)
                 #print(" ")
-                
-                datafile.write("%6.2f %1.0f %1.0f %5.2e %7.2e %7.2e %7.5f \n" % (a,b,c,d,e,ko,kij))
+                datafile.write("%6.2f %1.0f %1.0f %5.2e %7.2e %7.2e %7.5f \n" % (Wavelength,StatisticalWeightUpper,StatisticalWeightLower,Probability,LowerDensity,ko,kij))
     
-#reab_coeff_calc(n_ij, g_i, g_j, A, n_j)
-
-#print("")
-#print("ko and kij calculated and printed to file.")
-
-    
-
-# print("g_i 738 =",g_i[7],"g_j_738= ",g_j[7])
-# print("A_i 738 =",A[7], "n_ij = ", n_ij[7])
-# print("n_j 738 =", n_j[7])
-# print("loaded parameters from line_data2.txt")
-# print("ko738 = ")
-# print(ko_calc(n_ij[7],g_i[7],g_j[7],A[7]))
-
-#testing
-#k738 = ko[7]*n_j[7]*(T_g**-0.5)
-#print("k738 =", k738)
-    
-#os.rename("line_data3.txt", "line_data_full.txt")                                                            
-#os.rename("k_oresults.txt", time.strftime("Ko_%Y%m%d%H%M.txt")) 
-
-#ko_data(n_ij,g_i,g_j,A,n_j)
-    
-#dfd=pd.read_csv("line_data_full.txt",sep=" ",header=None,names=['Wavelength (nm)','g_i','g_j','A','n_j','k_o', 'k_ij'],comment="#")
-#dfd.to_csv('line_data_full.csv',sep=';',index=False)
 
 
         

combo.py
@@ -49,8 +49,8 @@ CharacteristicLength_str = str(CharacteristicLength)
 #Process Pressure, n_m, n_r
 
 #List of datafiles to be used in electron density calculations.
-filelist = ['RFPOWER0001',
-            'RFPOWER0011']
+filelist = ['RFPOWER0005']
+            #'RFPOWER0011']
             # 'RFPOWER0005',
             # 'RFPOWER0007',
             # 'RFPOWER0010']
@@ -430,12 +430,7 @@ def Te(T,n_m,n_r):
         resultsfile.write('Characteristic length (cm) = '+ CharacteristicLength_str + '\n')
     
     for a in T:
-                
-        #-----------------------------------   
-        #print('For an electron temperature of Te=',a)
-        #print('')
         R_lam, Rad = np.genfromtxt("Rad_TrapCoeff.csv", delimiter=';', unpack=True, skip_header=1, usecols=(0,1))
-        
         R_750 = Rad[0]
         R_772_4 = Rad[1]
         R_738 = Rad[2]
@@ -447,104 +442,44 @@ def Te(T,n_m,n_r):
         AverageRadTrap_772 = (R_772_4 + R_772_3)/2
         AverageRadTrap_750 = (R_750 + R_751)/2
         
-        
         #738/750
         LR_738 = LR_mod(kG_738,a,alphaG_738,EG_738,kM_738,alphaM_738,EM_738,kR_738,alphaR_738,ER_738,R_738,R_750,n_m,n_r)
-        #print("The 738/750 line ratio is ", LR_738, "for Te =", a)
-        #print("-------------------------")
-        
-        
-        
         LR_763 = LR_mod(kG_763,a,alphaG_763,EG_763,kM_763,alphaM_763,EM_763,kR_763,alphaR_763,ER_763,R_763,R_750,n_m,n_r)
-        #print("The 763/750 line ratio is, ", LR_763, "for Te =", a)
-        
-            #print("-------------------------")
-    
         LR_772 = LR_mod(kG_772,a,alphaG_772,EG_772,kM_772,alphaM_772,EM_772,kR_772,alphaR_772,ER_772,R_772_3,R_750,n_m,n_r)
-        #print("The 772/750 line ratio is, ", LR_772, "for Te =", a)
-        
-        #print("-------------------------")
-        
         LR_794 = LR_mod(kG_795,a,alphaG_795,EG_795,kM_795,alphaM_795,EM_795,kR_795,alphaR_795,ER_795,R_794,R_750,n_m,n_r)
-        #print("The 794/750 line ratio is, ", LR_794, "for Te =", a)
-        
         
         chi_sum = chi_squared(LR_738,Exp_738,0.05) + chi_squared(LR_763,Exp_763,0.05) + chi_squared(LR_772,Exp_772,0.1) + chi_squared(LR_794,Exp_794,0.1)
-        # print("")
-        # print("chi_sum for T =", a,"is: ",chi_sum)
-        # print("738 model LR is: ", LR_738, "and experimental 738 LR is: ", Exp_738)
         chi_738 = chi_squared(LR_738,Exp_738,0.05)
-        
-        #print("763 model LR is: ", LR_763, "and experimental 763 LR is: ", Exp_763)
         chi_763 = chi_squared(LR_763,Exp_763,0.05)
-        
-        #print("772 model LR is: ", LR_772, "and experimental 772 LR is: ", Exp_772)
-        chi_772 = chi_squared(LR_772,Exp_772,0.1)
-        
-        #print("794 model LR is: ", LR_794, "and experimental 794 LR is: ", Exp_794)
-        chi_794 = chi_squared(LR_794,Exp_794,0.1)
-        
-        #print("738 chi is: ", chi_738)
-        # print("763 chi is: ", chi_763)
-        # print("772 chi is: ", chi_772)
-        # print("794 chi is: ", chi_794)
-        
-        #Adding calculation of chi-sum without including 763nm line
-        
-        chi_excl = chi_738 + chi_772 + chi_794
-        
-        chi_s_e_738 = chi_763 + chi_772 + chi_794
-        
-        chi_s_e_both = chi_772 + chi_794
-        
-        chi_763_772 = chi_763 + chi_772
-        
-        chi_738_794 = chi_738 + chi_794
-        
-        chi_excl_772 = chi_738 + chi_794 + chi_763
+        chi_772 = chi_squared(LR_772,Exp_772,0.05)
+        chi_794 = chi_squared(LR_794,Exp_794,0.05)
+        chi_excl= chi_738 + chi_794 + chi_763
         
         with open('Te_results.txt', 'a+') as te_results:
             #for i in range(len(ResonantDensityIncm3)):
              #   mod_results.write("%d %d %d\n" % (ResonantDensityIncm3[i],MetastableDensityIncm3[i],chi_sum))
-            te_results.write("%4.2f %4.2f %4.2f %4.2f %4.2f %4.2f %4.2f %4.2f %4.2f %4.2f %4.2f %4.2f\n" % (a,chi_738, chi_763, chi_772, chi_794, chi_sum, chi_excl, chi_s_e_738, chi_s_e_both,chi_763_772, chi_738_794, chi_excl_772))
-            
+            te_results.write("%4.2f %4.2f %4.2f %4.2f %4.2f %4.2f %4.2f \n" % (a,chi_738, chi_763, chi_772, chi_794, chi_sum, chi_excl))    
         with open("LR_results.txt", 'a+') as lr_results:
             lr_results.write("%4.2f %5.3f %4.3f %4.3f %4.3f %4.3f %4.3f %4.3f %4.3f\n" % (a,LR_738,Exp_738,LR_763,Exp_763,LR_772,Exp_772,LR_794,Exp_794))
         
-    Chi_df=pd.read_csv("Te_results.txt",sep=" ",names=['Electron Temperature (eV)','738 Chi','763 Chi','772 Chi','794 Chi','Chi Sum', 'Chi Sum excl. 763nm', 'Chi Sum excl. 738nm', 'Chi Sum excl 738 and 763nm', 'Chi Sum 763 and 772', 'Chi Sum 738 and 794', 'Chi Sum excl 772nm'],comment="#",skiprows=6)
+    Chi_df=pd.read_csv("Te_results.txt",sep=" ",names=['Electron Temperature (eV)','738 Chi','763 Chi','772 Chi','794 Chi','Chi Sum','Chi Sum excl 772nm'],comment="#",skiprows=6)
     Chi_df.to_csv('Te_results.csv',sep=';',index=False)
     
     LR_df=pd.read_csv("LR_results.txt",sep=" ",header=None,names=['Electron Temperature (eV)','738 Model LR','738 Exp LR','763 Model LR','763 Exp LR','772 Model LR','772 Exp LR','794 Model LR','794 Exp LR'],comment='#',skiprows=6)
     LR_df.to_csv('LR_results.csv',sep=";",index=False)
                 
-    Te, chi_738, chi_763, chi_772, chi_794, chi_s, chi_s_excl, chi_s_excl_738, chi_s_excl_both, chi_763_772, chi_738_794, chi_excl_772 = np.genfromtxt("Te_results.csv", delimiter=";", skip_header=1, unpack=True, usecols=(0,1,2,3,4,5,6,7,8,9,10,11))
+    Te, chi_738, chi_763, chi_772, chi_794, chi_s, chi_s_excl = np.genfromtxt("Te_results.csv", delimiter=";", skip_header=1, unpack=True, usecols=(0,1,2,3,4,5,6))
     
     Te_l_lst = list(Te)
     chi_s_lst = list(chi_s)
-    
     chi_s_e_lst = list(chi_s_excl)
-    chi_s_e_738_lst = list(chi_s_excl_738)
-    chi_s_e_both_lst = list(chi_s_excl_both)
-    chi_763_772_lst = list(chi_763_772)
-    chi_738_794_lst = list(chi_738_794)
-    chi_excl_772_lst = list(chi_excl_772)
-    
     chi_738_lst = list(chi_738)
     chi_763_lst = list(chi_763)
     chi_772_lst = list(chi_772)
     chi_794_lst = list(chi_794)     
-    
     min_pos = chi_s_lst.index(min(chi_s_lst))
-    
     min_pos_excl = chi_s_e_lst.index(min(chi_s_e_lst))
-    
-    min_pos_738 = chi_s_e_738_lst.index(min(chi_s_e_738_lst))
-    
-    min_pos_both = chi_s_e_both_lst.index(min(chi_s_e_both_lst))
-    
-    min_763 = chi_763_lst.index(min(chi_763_lst))
-    
-    min_excl_772 = chi_excl_772_lst.index(min(chi_excl_772_lst))
+
     
     #plotting chi-sum 
     
@@ -566,16 +501,7 @@ def Te(T,n_m,n_r):
     ax5.plot(Te,chi_s, c='magenta',label='chi-sum')
     
     ax6 = fig.add_subplot(111)
-    ax6.plot(Te,chi_s_excl, c='orange',label='chi-sum excl 763')
-    
-    ax7 = fig.add_subplot(111)
-    ax7.plot(Te,chi_s_excl_738, c='purple',label='chi-sum excl 738')
-    
-    ax8 = fig.add_subplot(111)
-    ax8.plot(Te,chi_s_excl_both, c='black', label='chi-sum excl both')
-    
-    ax9 = fig.add_subplot(111)
-    ax9.plot(Te,chi_excl_772, c='brown',label='chi-sum excl 772')
+    ax6.plot(Te,chi_s_excl, c='orange',label='chi-sum excl 772')
     
     plt.legend(loc='upper right');
     
@@ -587,33 +513,27 @@ def Te(T,n_m,n_r):
     
     # print("The minimum in chi-squared, excluding 763 is:", chi_s_e_lst[min_pos_excl], "occuring when Te=", Te[min_pos_excl])
     
-    print("The minimum in chi-squared, excluding 738 is:", chi_s_e_738_lst[min_pos_738], "occuring when Te=", Te[min_pos_738])
+    print("The minimum in chi-squared, excluding 772 is:", chi_s_e_lst[min_pos_excl], "occuring when Te=", Te[min_pos_excl])
     
     #print("The minimum in chi-squared, excluding both 763 and 738 is:", chi_s_e_both_lst[min_pos_both], "occuring when Te=", Te[min_pos_both])
     
-    print("The minimum in chi squared for both 763 and 772 is: ", chi_763_772_lst[chi_763_772_lst.index(min(chi_763_772_lst))], "occuring when Te=", Te[chi_763_772_lst.index(min(chi_763_772_lst))])
-
-    print("The minimum in chi squared for both 738 and 794 is: ", chi_738_794_lst[chi_738_794_lst.index(min(chi_738_794_lst))], "occuring when Te=", Te[chi_738_794_lst.index(min(chi_738_794_lst))])
-    
-    print("The minimum in chi squared for all excl 772 is: ", chi_excl_772_lst[chi_excl_772_lst.index(min(chi_excl_772_lst))], "occuring when Te=", Te[chi_excl_772_lst.index(min(chi_excl_772_lst))])
-    
     print("The minimum in 738 chi is,", chi_738_lst[chi_738_lst.index(min(chi_738_lst))], "occuring when Te=", Te[chi_738_lst.index(min(chi_738_lst))])
     
-    print("The minimum in 763 chi is:", chi_763_lst[min_763], "occuring when Te=", Te[min_763])
+    print("The minimum in 763 chi is:", chi_763_lst[chi_763_lst.index(min(chi_763_lst))], "occuring when Te=", Te[chi_763_lst.index(min(chi_763_lst))])
     
     print("The minimum in 772 chi is,", chi_772_lst[chi_772_lst.index(min(chi_772_lst))], "occuring when Te=", Te[chi_772_lst.index(min(chi_772_lst))])
     
     print("The minimum in 794 chi is,", chi_794_lst[chi_794_lst.index(min(chi_794_lst))], "occuring when Te=", Te[chi_794_lst.index(min(chi_794_lst))])
     
-    os.rename("Te_results.txt", time.strftime("TeResults/te_results"+File+param+GasTemperatureInKelvin_str+"K_%Y%m%d%H%M%S.txt")) 
-    os.rename("Te_results.csv", time.strftime("TeResults/te_results"+File+param+GasTemperatureInKelvin_str+"_%Y%m%d%H%M%S.csv"))
+    # os.rename("Te_results.txt", time.strftime("TeResults/te_results"+File+param+GasTemperatureInKelvin_str+"K_%Y%m%d%H%M%S.txt")) 
+    # os.rename("Te_results.csv", time.strftime("TeResults/te_results"+File+param+GasTemperatureInKelvin_str+"_%Y%m%d%H%M%S.csv"))
     
-    os.rename("LR_results.txt", time.strftime("TeResults/LR_results"+File+param+GasTemperatureInKelvin_str+"_%Y%m%d%H%M%S.txt")) 
-    os.rename("LR_results.csv", time.strftime("TeResults/LR_results"+File+param+GasTemperatureInKelvin_str+"_%Y%m%d%H%M%S.csv"))
+    # os.rename("LR_results.txt", time.strftime("TeResults/LR_results"+File+param+GasTemperatureInKelvin_str+"_%Y%m%d%H%M%S.txt")) 
+    # os.rename("LR_results.csv", time.strftime("TeResults/LR_results"+File+param+GasTemperatureInKelvin_str+"_%Y%m%d%H%M%S.csv"))
     
     #os.remove("te_results.csv")
     
-    final_T = [Te[min_pos],Te[chi_763_772_lst.index(min(chi_763_772_lst))],Te[chi_738_794_lst.index(min(chi_738_794_lst))],Te[min_pos_738]]
+    final_T = [Te[min_pos],Te[min_pos_excl]]
     
     return final_T
 
@@ -705,7 +625,7 @@ for File in filelist:
     
         #Ask user for electon temperature value to be used in electron density calculation
         #calling integrate function to determine intensity of 451 and 750 emission lines
-        os.chdir(r'C:\Users\beau.bussell\Google Drive\EngD\Research Data\OES\Calibrated\181220')
+        os.chdir(r'C:\Users\beaub\Google Drive\EngD\Research Data\OES\Calibrated\181220')
         
         SpectraResult = FetchSpectra(File)
         Wavelength = SpectraResult[0]
@@ -782,11 +702,7 @@ for File in filelist:
         
         sumT = final_T[0]
         
-        T_763_772 = final_T[1]
-        
-        T_738_794 = final_T[2]
-        
-        Te_excl_738 = final_T[3]
+        T_excl_772 = final_T[1]
         
         print("--------------------------------------------------------------")
         print("")
@@ -797,7 +713,7 @@ for File in filelist:
         print("The time taken to measure electron temperature is: ", current_time)
         pause()
         
-        e_density(sumT,File,"Tsum")
+        e_density(T_excl_772,File,"Tsum")
         
         #e_density(T_763_772,File,"T_763_772")
         